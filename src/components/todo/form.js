93e3ed618f8c44657a364bb40bb8d12ca86077c0
Yuliya Barysevich <barysevich.yuliya@gmail.com>
2021-06-01 21:09:17 -0400

    changed form using bootstrap-react

--- src/components/todo/form.js
+++ src/components/todo/form.js
@@ -1,5 +1,11 @@
 import React, { useEffect, useState } from 'react';
 
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 function TodoForm(props) {
   const [item, setItem] = useState({})
 
@@ -16,23 +22,50 @@ function TodoForm(props) {
 
   return (
     <>
-        <h3>Add Item</h3>
-        <form onSubmit={handleSubmit}>
-          <label>
-            <span>To Do Item</span>
-            <input name="text" placeholder="Add To Do List Item" onChange={_handleInputChange} />
-          </label>
-          <label>
-            <span>Assigned To</span>
-            <input name="text" placeholder="Assignee Name" onChange={_handleInputChange} />
-          </label>
-          <label>
-            <span>Difficulty Rating</span>
-            <input defaultValue="1" type="range" min="1" max="5" name="difficulty" onChange={_handleInputChange} />
-          </label>
-          <button>Add Item</button>
-        </form>
-      </>
+    <Form onSubmit={handleSubmit}>
+      <Card>
+        <Card.Body>
+          <Card.Title>Add To Do Item</Card.Title>
+
+          <Form.Group>
+            <Form.Label>To Do Item</Form.Label>
+            <Form.Control name="text"  placeholder="Add To Do List Item" onChange={_handleInputChange}/>
+          </Form.Group>
+
+          <Form.Group>
+            <Form.Label>Assigned To</Form.Label>
+            <Form.Control type="text"  placeholder="Assignee Name" onChange={_handleInputChange}/>
+          </Form.Group>
+
+          <Form.Group>
+            <Form.Label>Difficulty Rating</Form.Label>
+            <Form.Control type="range" onChange={_handleInputChange}/>
+          </Form.Group>
+
+          <Button variant="primary" type="submit">Add Item</Button>
+
+        </Card.Body>
+      </Card>
+    </Form>
+    </>
+    // <>
+    //     <h3>Add Item</h3>
+    //     <form onSubmit={handleSubmit}>
+    //       <label>
+    //         <span>To Do Item</span>
+    //         <input name="text" placeholder="Add To Do List Item" onChange={_handleInputChange} />
+    //       </label>
+    //       <label>
+    //         <span>Assigned To</span>
+    //         <input name="text" placeholder="Assignee Name" onChange={_handleInputChange} />
+    //       </label>
+    //       <label>
+    //         <span>Difficulty Rating</span>
+    //         <input defaultValue="1" type="range" min="1" max="5" name="difficulty" onChange={_handleInputChange} />
+    //       </label>
+    //       <button>Add Item</button>
+    //     </form>
+    //   </>
 
   )
 }
